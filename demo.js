539a05fadbbe3f50f9064c969e329194779e225b
Ekta <ekta@mapzen.com>
2014-12-22 17:00:26 -0500

    baahhh

---
+++ demo.js
@@ -0,0 +1,14 @@
+  var map = L.map('map', {
+      zoom: $rootScope.geobase.zoom,
+      zoomControl: false,
+      center: [$rootScope.geobase.lat, $rootScope.geobase.lon]
+  });
+
+  L.tileLayer('//{s}.tiles.mapbox.com/v3/randyme.h29f04e6/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+  }).addTo(map);
+
+  L.geoJson(lighthouse).addTo(map);
+
+  L.marker().addTo(map);
---
+++ lighthouses.js
@@ -0,0 +1,20 @@
+var lighthouse = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    29.885556,
+                    -81.288611
+                ]
+            },
+            "properties": {
+                "name": "St. Augustine Light",
+                "location": "Florida, USA",
+                "popupContent": "Welcome to Florida!"
+            },
+        }
+    ]
+};
